f2d144a93ebcf837267ac5c83c435e42c00f01b1
linn <673879891@qq.com>
2021-12-15 11:18:45 +0800

    add backend and frontend

---
+++ vue-flask/README.md
@@ -0,0 +1,20 @@
+> backend
+
+run
+``` bash
+flask run
+```
+or 
+``` bash
+python3 app.py
+```
+> frontend
+
+install packages
+``` bash
+cnpm install
+```
+run
+```
+npm run dev
+```
---
+++ vue-flask/frontend/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>{{ datetime }}</h1>
+  </div></template
+>
+<script>
+export default {
+  name: 'App',
+  data () {
+    return { datetime: 'ee' }
+  },
+  methods: {
+    initData () {
+      this.$ajax.get('/time', {}).then(res => {
+        this.datetime = res.data.time
+      })
+    }
+  },
+  mounted () {
+    this.initData()
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
